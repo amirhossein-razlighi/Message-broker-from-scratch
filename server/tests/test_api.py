821fed249d10f894317d0b3e2bcafd39dcc57a1c
AmirHossein-nr <arazlighi@gmail.com>
2024-02-10 14:29:41 +0330

    update test_api

--- server/tests/test_api.py
+++ server/tests/test_api.py
@@ -11,11 +11,11 @@ from server.broker import Broker
 
 class TestAPI(unittest.TestCase):
     def test_api_sanity(self):
-        broker = Broker("127.0.0.1", 8888, 8000, 7500)
+        broker = Broker("127.0.0.1", 8001, 8002, 8003)
         broker_thread = threading.Thread(target=broker.run, daemon=True)
         broker_thread.start()
         sleep(1)
         host = "127.0.0.1"
-        port = 8000
+        port = 8001
         response = requests.get(f"http://{host}:{port}/zookeeper")
         self.assertEqual(response.status_code, 200)
