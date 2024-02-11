72c7b32d60c9126c6302964d4d2d50c40db4c871
hasti <hastikarimi2002@gmail.com>
2024-02-11 23:53:39 +0330

    update: build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-cd server
+cd ./server
 sudo docker build -t server_app:latest -f Dockerfile.broker .
 sudo docker build -t zookeeper_app:latest -f Dockerfile.zookeeper .
 cd ../client
-sudo docker build . -t client_app:latest
+sudo docker build -t client_app:latest .
 cd ..
